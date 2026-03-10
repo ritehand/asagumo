cf1c16b25ac3a3b200911a771be05917b8fdd4d9
1l0 <rh.plus001@gmail.com>
2026-03-10 17:06:20 +0900

    tidy env

--- env.go
+++ env.go
@@ -6,22 +6,18 @@ import (
 )
 
 var (
-	AppID     string
-	PublicKey string
-	GuildID   string
-	Token     string
+	Token   string
+	GuildID string
 )
 
 func init() {
-	AppID = os.Getenv("ASAGUMO_APP_ID")
-	PublicKey = os.Getenv("ASAGUMO_PUBLIC_KEY")
-	GuildID = os.Getenv("ASAGUMO_GUILD_ID")
 	if _, ok := os.LookupEnv("DEV"); ok {
 		Token = os.Getenv("ASAGUMO_TOKEN_DEV")
 	} else {
 		Token = os.Getenv("ASAGUMO_TOKEN")
 	}
-	if AppID == "" || PublicKey == "" || GuildID == "" || Token == "" {
+	GuildID = os.Getenv("ASAGUMO_GUILD_ID")
+	if Token == "" || GuildID == "" {
 		log.Fatalln("Missing environment variables")
 	}
 }
